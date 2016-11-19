b450a2f0415c55e602db5ac6d3c4ef2bd9619452
amiya <mahapatro.amiya@gmail.com>
2016-11-19 17:34:08 +0530

    this avhdskhfdk

--- HibernateSampleExample/src/test/java/com/javawebtutor/HibernateUtil.java
+++ HibernateSampleExample/src/test/java/com/javawebtutor/HibernateUtil.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
 	private static final SessionFactory sessionFactory = buildSessionFactory();
 
-	private static SessionFactory buildSessionFactory() {
+	private static SessionFactory buildSessionFactory() {//kjsagjhagdsj
 		try {
 			// Create the SessionFactory from hibernate.cfg.xml
 			return new Configuration().configure().buildSessionFactory();
